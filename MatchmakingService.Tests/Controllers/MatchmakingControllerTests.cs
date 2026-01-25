c21b4fbe1db402fed0e1042e7943282999337678
best-koder-ever <dettaminnsjag@gmail.com>
2026-01-25 12:09:52 +0100

    feat(T003): Add test skeletons for MatchmakingService

---
+++ MatchmakingService.Tests/Controllers/MatchmakingControllerTests.cs
@@ -0,0 +1,7 @@
+using Xunit;
+namespace MatchmakingService.Tests.Controllers;
+public class MatchmakingControllerTests {
+    [Fact(Skip="T003")] public async Task HandleMutualMatch_ValidRequest_CreatesMatch() { }
+    [Fact(Skip="T003")] public async Task FindMatches_ValidRequest_ReturnsMatches() { }
+    [Fact(Skip="T003")] public async Task HealthCheck_ReturnsHealthy() { }
+}
